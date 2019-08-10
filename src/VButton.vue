621d57da1110d0de0d766e43ce4ef007ce409c56
har <istarcoming@163.com>
2019-08-11 00:30:03 +0800

    添加button组件

---
+++ src/VButton.vue
@@ -0,0 +1,91 @@
+<template>
+    <button class="v-button"
+            :class="{ [`icon-${iconPosition}`]: true }"
+            @click="$emit('click')">
+        <v-icon class="loading icon" v-if="loading" name="loading"></v-icon>
+        <v-icon class="icon" v-if="icon && !loading" :name="icon"></v-icon>
+        <span class="text"><slot /></span>
+    </button>
+</template>
+
+<script>
+import VIcon from './VIcon'
+
+export default {
+    name: 'VButton',
+    props: {
+        icon: {
+            type: String,
+            default: ''
+        },
+        iconPosition: {
+            type: String,
+            default: 'left',
+            validator (val) {
+                return val === 'left' || val === 'right'
+            }
+        },
+        loading: {
+            type: Boolean,
+            default: false
+        }
+    },
+    components: {
+        VIcon
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+.v-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    align-items: center;
+    vertical-align: top;
+
+    .text {
+        order: 1;
+    }
+
+    .icon {
+        margin-right: 4px;
+
+        &.loading {
+            animation: spin 2s infinite linear;
+        }
+    }
+
+    &.icon-right .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: 4px;
+    }
+
+    &:hover {
+        border-color: var(--border-color-hover);
+    }
+
+    &:active {
+        background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+        outline: none;
+    }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
+import VButton from './VButton'
+import VIcon from './VIcon'
+
+Vue.component('v-button', VButton)
+Vue.component('v-icon', VIcon)
 
 new Vue({
     el: '#app',
