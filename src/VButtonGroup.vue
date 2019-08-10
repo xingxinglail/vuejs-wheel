af2d830d6de0dd7fdcf3d947fc7353cce90e61d2
har <istarcoming@163.com>
2019-08-11 00:41:21 +0800

    添加button-group组件

---
+++ src/VButtonGroup.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="v-button-group">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VButtonGroup',
+    mounted () {
+        for (let node of this.$el.children) {
+            let name = node.nodeName.toLowerCase()
+            if (name !== 'button') {
+                console.warn(`v-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`)
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+
+    .v-button {
+        border-radius: 0;
+
+        &:not(:first-child) {
+            margin-left: -1px;
+        }
+
+        &:first-child {
+            border-radius: var(--border-radius) 0 0 var(--border-radius);
+        }
+
+        &:last-child {
+            border-radius: 0 var(--border-radius) var(--border-radius) 0;
+        }
+
+        &:hover {
+            position: relative;
+            z-index: 1;
+        }
+    }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import VButton from './VButton'
 import VIcon from './VIcon'
+import VButtonGroup from './VButtonGroup'
 
 Vue.component('v-button', VButton)
 Vue.component('v-icon', VIcon)
+Vue.component('v-button-group', VButtonGroup)
 
 new Vue({
-    el: '#app',
-    data: {
-        test: '1'
-    }
+    el: '#app'
 })
