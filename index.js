813c1a689a3b5bba04e847a703816e22da8dcbf7
har <istarcoming@163.com>
2019-08-11 15:23:38 +0800

    添加npm发布配置文件

---
+++ index.js
@@ -0,0 +1,9 @@
+import VButton from './src/VButton'
+import VButtonGroup from './src/VButtonGroup'
+import VIcon from './src/VIcon'
+
+export default {
+    VButton,
+    VButtonGroup,
+    VIcon
+}
