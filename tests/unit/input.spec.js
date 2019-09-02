d0d1259b7c0b17781b3b112706fd110f8ae3513b
har <istarcoming@163.com>
2019-09-02 16:41:52 +0800

    test(input): 升级测试用例

---
+++ tests/unit/input.spec.js
@@ -0,0 +1,71 @@
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import VInput from '../../src/VInput'
+import sinon from "sinon";
+
+describe('Input', () => {
+
+    it('存在', () => {
+        expect(VInput).to.exist
+    })
+
+    describe('props', () => {
+
+        it('接收value', () => {
+            const wrapper = mount(VInput, {
+                propsData: {
+                    value: 'hi'
+                }
+            })
+            expect(wrapper.props('value')).to.eq('hi')
+            expect(wrapper.vm.$el.querySelector('input').value).to.eq('hi')
+        })
+
+        it('接收disabled', () => {
+            const wrapper = mount(VInput, {
+                propsData: {
+                    disabled: true
+                }
+            })
+            expect(wrapper.props('disabled')).to.eq(true)
+            expect(wrapper.vm.$el.querySelector('input').disabled).to.eq(true)
+        })
+
+        it('接收readonly', () => {
+            const wrapper = mount(VInput, {
+                propsData: {
+                    readonly: true
+                }
+            })
+            expect(wrapper.props('readonly')).to.eq(true)
+            expect(wrapper.vm.$el.querySelector('input').readOnly).to.eq(true)
+        })
+
+        it('接收error', () => {
+            const wrapper = mount(VInput, {
+                propsData: {
+                    error: '出错了！'
+                }
+            })
+            expect(wrapper.props('error')).to.eq('出错了！')
+            const useDom = wrapper.find('use')
+            expect(useDom.attributes('href')).to.eq('#i-info-circle')
+            const spanDom = wrapper.find('span')
+            expect(spanDom.text()).to.eq('出错了！')
+        })
+    })
+
+    describe('事件', () => {
+
+        it('input/change/focus/blur', () => {
+            const wrapper = mount(VInput)
+            const input = wrapper.find('input')
+            ;['input', 'change', 'focus', 'blur'].forEach(eventName => {
+                const handler = sinon.stub()
+                wrapper.vm.$on(eventName, handler)
+                input.trigger(eventName)
+                expect(handler.called).to.eq(true)
+            })
+        })
+    })
+})
